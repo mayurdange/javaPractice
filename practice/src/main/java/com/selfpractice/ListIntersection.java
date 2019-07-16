0c4b571d6cc1e6b0c8e319e6ad4eee05b81ca411
747610 <747610@cognizant.com>
2019-07-16 22:37:57 +0530

    added assignment junit test to find intersection of 2 lists within 40ms

---
+++ practice/src/main/java/com/selfpractice/ListIntersection.java
@@ -0,0 +1,10 @@
+package com.selfpractice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ListIntersection {
+    public List<String> intersect(List<String> list1,List<String> list2){
+        return list1.parallelStream().filter(list2::contains).collect(Collectors.toList());
+    }
+}
---
+++ practice/src/test/java/com/selfpractice/ListIntersectionTest.java
@@ -0,0 +1,38 @@
+package com.selfpractice;
+
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class ListIntersectionTest {
+    private static final long EXPECTED_DURATION = TimeUnit.NANOSECONDS.convert(40, TimeUnit.MILLISECONDS);
+    private ListIntersection listIntersection = new ListIntersection();
+    private static final int MAX_ARR = 10000;
+
+    @Test
+    public void testListIntersectionWithPerf() {
+        List<String> l1 = ThreadLocalRandom.current().ints().limit(MAX_ARR).mapToObj(i -> Integer.toString(i))
+                .collect(Collectors.toList());
+        List<String> l2 = ThreadLocalRandom.current().ints().limit(MAX_ARR).mapToObj(i -> Integer.toString(i))
+                .collect(Collectors.toList());
+        long startTime = System.nanoTime();
+        List<String> receivedList = listIntersection.intersect(l1, l2);
+        long timeTaken = System.nanoTime() - startTime;
+        long timeTakenInMilis = TimeUnit.MILLISECONDS.convert(timeTaken, TimeUnit.NANOSECONDS);
+        System.out.println("total time taken" + timeTakenInMilis);
+        Assert.assertTrue("took too much time " + timeTakenInMilis + " ms",
+                timeTaken < EXPECTED_DURATION);
+        List<String> expectedList = l1.stream().filter(l2::contains).collect(Collectors.toList());
+        expectedList.forEach((l)->{
+            Assert.assertTrue(receivedList.contains(l));
+        });
+	}
+
+}
