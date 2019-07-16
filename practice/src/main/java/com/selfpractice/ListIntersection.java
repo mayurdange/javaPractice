8f39c7e3626978f172d599e323f68d2f7bd49f1a
747610 <747610@cognizant.com>
2019-07-16 22:58:06 +0530

    to trigger build

--- practice/src/main/java/com/selfpractice/ListIntersection.java
+++ practice/src/main/java/com/selfpractice/ListIntersection.java
@@ -3,7 +3,7 @@ package com.selfpractice;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class ListIntersection {
+public class ListIntersection { 
     public List<String> intersect(List<String> list1,List<String> list2){
         return list1.parallelStream().filter(list2::contains).collect(Collectors.toList());
     }
