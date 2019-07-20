6c2584a9d462e495a41c1b56ff4e47be44a933a7
mayur <mayurdange@live.com>
2019-07-20 17:58:28 +0000

    List intersection using hashing

--- practice/src/main/java/com/selfpractice/ListIntersection.java
+++ practice/src/main/java/com/selfpractice/ListIntersection.java
@@ -1,10 +1,16 @@
 package com.selfpractice;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
 
-public class ListIntersection { 
+public class ListIntersection {
     public List<String> intersect(List<String> list1,List<String> list2){
-        return list1.parallelStream().filter(list2::contains).collect(Collectors.toList());
+    HashSet<String> h1 = new HashSet<String>(list1);
+    HashSet<String> h2 = new HashSet<String>(list2);
+
+    h1.retainAll(h2);
+    List<String> l1 = new ArrayList<String>(h1); 
+      return l1;
     }
 }
