6d2e380ee0f57a59801e0218c6fadde23340f1df
Mayur <mayurdange@yahoo.co.in>
2019-07-24 23:12:19 +0530

    trying to add java classes .. resolving errors

---
+++ practice/src/main/java/com/selfpractice/Main.java
@@ -0,0 +1,12 @@
+package com.selfpractice;
+
+import com.selfpractice.collections.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ListIntersection l = new ListIntersection();
+		l.intersect(Arrays.asList("a","b"), Arrays.asList("b","c"));
+	}
+
+}
